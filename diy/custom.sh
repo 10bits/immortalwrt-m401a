8777e9431e36a598c5a888790e35324c99d502f3
10bits <loseher2020@outlook.com>
2022-11-08 11:04:10 +0800

    update

--- diy/custom.sh
+++ diy/custom.sh
@@ -8,3 +8,6 @@
 # Set etc/openwrt_release
 # sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 # echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.60.123/g' package/base-files/files/bin/config_generate
